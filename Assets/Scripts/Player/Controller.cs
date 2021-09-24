bd9ab0710700f9eb1f45e6743d21bd20bf6b95f5
JustSomeDude2001 <kim.yaroslavik@gmail.com>
2021-09-24 19:10:48 +0500

    Finalizing conversion to 3D

--- Assets/Scripts/Player/Controller.cs
+++ Assets/Scripts/Player/Controller.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class Controller : MonoBehaviour
 {
     private Rigidbody selfRigidbody;
+    private Gyroscope selfGyroscope;
+
+    private Vector3 screenCenterPoint;
 
     /*
         0 - Keyboard (testing)
@@ -21,6 +24,19 @@ public class Controller : MonoBehaviour
     void OnEnable()
     {
         selfRigidbody = GetComponent<Rigidbody>();
+        switch (controllerType) {
+            case 0:
+                break;
+            case 1:
+                selfGyroscope = Input.gyro;
+                selfGyroscope.enabled = true;
+                break;
+            case 2:
+                screenCenterPoint = new Vector3(Screen.width / 2, Screen.height / 2, 0);
+                Debug.Log(screenCenterPoint);
+                break;
+
+        }
     }
 
     Vector3 getKeyboardInput() {
@@ -33,15 +49,35 @@ public class Controller : MonoBehaviour
         return Vector3.zero;
     }
 
+    Vector3 getGyroscopeInput() {
+        return new Vector3(selfGyroscope.gravity.x, 0, 0);
+    }
+
+    Vector3 getTouchscreenInput() {
+        if (Input.touchCount > 0) {
+
+            Touch newTouch = Input.GetTouch(0);
+
+            Vector3 normalizedMovement = new Vector3(newTouch.position.x, newTouch.position.y, 0);
+
+            normalizedMovement.x = (normalizedMovement.x - screenCenterPoint.x) / screenCenterPoint.x;
+            normalizedMovement.y = (normalizedMovement.y - screenCenterPoint.y) / screenCenterPoint.y;
+
+            return normalizedMovement;
+        }
+
+        return Vector3.zero;
+    }
+
     // Input can wary, hence specific function.
     Vector3 getInput() {
         switch (controllerType) {
             case 0:
                 return getKeyboardInput();
             case 1:
-                break;
+                return getGyroscopeInput();
             case 2:
-                break;
+                return getTouchscreenInput();
         }
 
         return Vector2.zero;
